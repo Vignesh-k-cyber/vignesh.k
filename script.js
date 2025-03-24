0c7e62c39a177073fd9f33604fcabbd536a327a3
Vignesh-k-cyber <vignesh.k.0429@gmail.com>
2025-03-25 00:12:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.querySelectorAll('.box').forEach(box => {
-    box.addEventListener('mouseenter', () => {
-        box.style.boxShadow = "0px 0px 10px #00bcd4";
+document.querySelectorAll(".box").forEach(box => {
+    box.addEventListener("mouseenter", () => {
+        box.style.transform = "scale(1.05)";
+        box.style.transition = "0.3s ease-in-out";
     });
 
-    box.addEventListener('mouseleave', () => {
-        box.style.boxShadow = "none";
+    box.addEventListener("mouseleave", () => {
+        box.style.transform = "scale(1)";
     });
 });
-
