32736d9dd52acb64705eaa045e49e9a5197bf982
Vignesh-k-cyber <vignesh.k.0429@gmail.com>
2025-03-24 16:47:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll('.box').forEach(box => {
         box.addEventListener("mouseover", function () {
-            this.style.boxShadow = "0px 0px 15px rgba(0, 255, 255, 0.8)";
+            this.style.boxShadow = "0px 0px 15px rgba(106, 0, 106, 0.8)"; /* Blended Dark Purple */
         });
         box.addEventListener("mouseout", function () {
-            this.style.boxShadow = "0px 0px 10px rgba(0, 255, 255, 0.5)";
+            this.style.boxShadow = "0px 0px 10px rgba(106, 0, 106, 0.5)"; /* Blended Dark Purple */
         });
     });
 });
