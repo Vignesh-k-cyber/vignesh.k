b1af1cdf67cb0265ba6c986db8cca0856fff4593
Vignesh-k-cyber <vignesh.k.0429@gmail.com>
2025-03-27 12:14:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function viewCertificate(path) {
-    window.open(path, '_blank');
-}
+document.querySelectorAll('.cert-btn').forEach(button => {
+    button.addEventListener('click', function () {
+        document.getElementById('certificate-frame').src = this.getAttribute('data-cert');
+        document.getElementById('certificate-modal').style.display = 'block';
+    });
+});
+
+document.querySelector('.close-btn').addEventListener('click', function () {
+    document.getElementById('certificate-modal').style.display = 'none';
+});
